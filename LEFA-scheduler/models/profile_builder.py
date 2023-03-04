acd4a50d95def8efab16d271f8b00f4bde8769f6
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-04 14:16:30 -0800

    Create profile builder shell

---
+++ LEFA-scheduler/models/profile_builder.py
@@ -0,0 +1,14 @@
+import csv
+
+class ProfileBuilder:
+
+	def __init__(self, csv_path):
+		self.csv_path = csv_path
+
+
+	def build_instructor_profiles(self):
+		profiles = self.create_profiles()
+
+		return instructors
+
+	
