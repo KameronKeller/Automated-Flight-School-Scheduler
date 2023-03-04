f6fdcb56133d3ce216a0126fd6b032a0b3ee5776
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-03 16:42:21 -0800

    Create person class

---
+++ LEFA-scheduler/models/person.py
@@ -0,0 +1,13 @@
+class Person:
+
+	def __init__(self, first_name, last_name, unavailability):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.unavailability = self.parse_unavailability(unavailability)
+
+	def parse_unavailability(self, unavailability):
+		parsed_unavailability = {}
+		for day, times in unavailability.items():
+			parsed_unavailability[day] = times.split(';')
+
+		return parsed_unavailability
