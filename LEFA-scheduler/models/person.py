00a6f3fead4c8f6f7fe47d8cba2a2543cc35c152
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-04 16:59:54 -0800

    Sketch out person class

--- LEFA-scheduler/models/person.py
+++ LEFA-scheduler/models/person.py
@@ -3,11 +3,12 @@ class Person:
 	def __init__(self, first_name, last_name, unavailability):
 		self.first_name = first_name
 		self.last_name = last_name
-		self.unavailability = self.parse_unavailability(unavailability)
+		# self.unavailability = self.parse_unavailability(unavailability)
+		self.unavailability = unavailability
 
-	def parse_unavailability(self, unavailability):
-		parsed_unavailability = {}
-		for day, times in unavailability.items():
-			parsed_unavailability[day] = times.split(';')
+	# def parse_unavailability(self, unavailability):
+	# 	parsed_unavailability = {}
+	# 	for day, times in unavailability.items():
+	# 		parsed_unavailability[day] = times.split(';')
 
-		return parsed_unavailability
+	# 	return parsed_unavailability
