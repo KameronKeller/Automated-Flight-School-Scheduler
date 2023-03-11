2516b75e4a552ecb3445d7cb279c01ffd55a028b
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-11 15:28:39 -0800

    Build out instructor object

--- LEFA-scheduler/models/instructor.py
+++ LEFA-scheduler/models/instructor.py
@@ -1,4 +1,10 @@
+from models.person import Person
+
 class Instructor(Person):
 
-	def __init__(self):
-		self.students = None
+	def __init__(self, first_name, last_name, unavailability):
+		super().__init__(first_name, last_name, unavailability)
+		self.students = []
+
+	def add_student(self, student):
+		self.students.append(student)
