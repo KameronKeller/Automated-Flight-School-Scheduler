49ec20571307fe0b35d017b22c448ff56b075430
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-11 15:28:45 -0800

    Add last name to person

--- LEFA-scheduler/models/person.py
+++ LEFA-scheduler/models/person.py
@@ -3,6 +3,7 @@ class Person:
 	def __init__(self, first_name, last_name, unavailability):
 		self.first_name = first_name
 		self.last_name = last_name
+		self.full_name = first_name + ' ' + last_name
 		# self.unavailability = self.parse_unavailability(unavailability)
 		self.unavailability = unavailability
 
