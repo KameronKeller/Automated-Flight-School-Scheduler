189db7b7a1e3e7e9186a66061a03aaa421d50bfc
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-16 16:44:15 -0700

    Initial commit

---
+++ LEFA-scheduler/models/schedule_builder.py
@@ -0,0 +1,11 @@
+from models.instructor import Instructor
+# from models.aircraft import Aircraft
+
+class ScheduleBuilder:
+
+	# def __init__(self, instructors, aircraft):
+	def __init__(self, instructors):
+		self.instructors = instructors
+		# self.aircraft = aircraft
+
+
