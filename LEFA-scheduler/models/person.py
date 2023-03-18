e56594efd8901fce74e8c205aab6a3cd8150daa9
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-18 12:12:36 -0700

    Implement unavailability parser

--- LEFA-scheduler/models/person.py
+++ LEFA-scheduler/models/person.py
@@ -4,12 +4,23 @@ class Person:
 		self.first_name = first_name
 		self.last_name = last_name
 		self.full_name = first_name + ' ' + last_name
-		# self.unavailability = self.parse_unavailability(unavailability)
-		self.unavailability = unavailability
+		self.unavailability = self.parse_unavailability(unavailability)
+		# self.unavailability = unavailability
+		# print(self.unavailability)
 
-	# def parse_unavailability(self, unavailability):
-	# 	parsed_unavailability = {}
-	# 	for day, times in unavailability.items():
-	# 		parsed_unavailability[day] = times.split(';')
+	def parse_unavailability(self, unavailability):
+		parsed_unavailability = {}
+		# print(unavailability)
+		for day, times in unavailability.items():
+			converted_times = []
+			split_times = times.split(';')
+			for time in split_times:
+				if len(time) == 0:
+					continue
+				else:
+					hour = int(time.split(':')[0])
+					converted_times.append(hour)
+			parsed_unavailability[day] = converted_times
+			print(converted_times)
 
-	# 	return parsed_unavailability
+		return parsed_unavailability
