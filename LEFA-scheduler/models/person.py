713868c5198a699eba7691e0f498f4a9fd07f5f0
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-18 13:22:24 -0700

    Use a set instead of a list for storing unavailable times

--- LEFA-scheduler/models/person.py
+++ LEFA-scheduler/models/person.py
@@ -5,22 +5,18 @@ class Person:
 		self.last_name = last_name
 		self.full_name = first_name + ' ' + last_name
 		self.unavailability = self.parse_unavailability(unavailability)
-		# self.unavailability = unavailability
-		# print(self.unavailability)
 
 	def parse_unavailability(self, unavailability):
 		parsed_unavailability = {}
-		# print(unavailability)
 		for day, times in unavailability.items():
-			converted_times = []
+			converted_times = set()
 			split_times = times.split(';')
 			for time in split_times:
 				if len(time) == 0:
 					continue
 				else:
 					hour = int(time.split(':')[0])
-					converted_times.append(hour)
+					converted_times.add(hour)
 			parsed_unavailability[day] = converted_times
-			print(converted_times)
 
 		return parsed_unavailability
