193e0bde279d2f883c4de928bd655d42f3c66f24
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-18 20:31:49 -0700

    Update student factory

--- LEFA-scheduler/tests/factory/student_factory.py
+++ LEFA-scheduler/tests/factory/student_factory.py
@@ -4,13 +4,14 @@ class StudentFactory:
 
 	first_name = 'first'
 	last_name = 'last'
-	unavailability = {'fixedwing_friday_unavailability': '05:30;06:30;',
-	'fixedwing_monday_unavailability': '06:30;05:30;09:30;11:30;10:30;',
-	'fixedwing_saturday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_sunday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_thursday_unavailability': '05:30;06:30;',
-	'fixedwing_tuesday_unavailability': '06:30;05:30;',
-	'fixedwing_wednesday_unavailability': '05:30;06:30;'}
+	unavailability = {
+	'Sunday' : '05:30;06:30;',
+	'Monday' : '06:30;05:30;09:30;11:30;10:30;',
+	'Tuesday' : '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
+	'Wednesday' : '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
+	'Thursday' : '05:30;06:30;',
+	'Friday' : '06:30;05:30;',
+	'Saturday' : '05:30;06:30;'}
 	rating = 'rating'
 	schedule_type = 'schedule_type'
 	instructor = 'instructor'
