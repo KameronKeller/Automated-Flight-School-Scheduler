c95dde0343913f9fa32b58b3eaafab85b9559c0f
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-18 20:31:43 -0700

    Update instructor factory

--- LEFA-scheduler/tests/factory/instructor_factory.py
+++ LEFA-scheduler/tests/factory/instructor_factory.py
@@ -2,16 +2,30 @@ from models.instructor import Instructor
 
 class InstructorFactory:
 
-	first_name = 'first'
-	last_name = 'last'
-	unavailability = {'fixedwing_friday_unavailability': '05:30;06:30;',
-	'fixedwing_monday_unavailability': '06:30;05:30;09:30;11:30;10:30;',
-	'fixedwing_saturday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_sunday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_thursday_unavailability': '05:30;06:30;',
-	'fixedwing_tuesday_unavailability': '06:30;05:30;',
-	'fixedwing_wednesday_unavailability': '05:30;06:30;'}
+	first_name = 'Instructor'
+	last_name = 'L'
+	unavailability = {
+	'Sunday' : '05:30;06:30;',
+	'Monday' : '06:30;05:30;09:30;11:30;10:30;',
+	'Tuesday' : '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
+	'Wednesday' : '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
+	'Thursday' : '05:30;06:30;',
+	'Friday' : '06:30;05:30;',
+	'Saturday' : '05:30;06:30;'}
+
+	completely_free = {
+	'Sunday': '',
+	'Monday': '',
+	'Tuesday': '',
+	'Wednesday': '',
+	'Thursday': '',
+	'Friday': '',
+	'Saturday': ''}
 
 	@staticmethod
 	def create_instructor(first_name=first_name, last_name=last_name, unavailability=unavailability):
+		return Instructor(first_name, last_name, unavailability)
+
+	@staticmethod
+	def create_free_instructor(first_name=first_name, last_name=last_name, unavailability=completely_free):
 		return Instructor(first_name, last_name, unavailability)
