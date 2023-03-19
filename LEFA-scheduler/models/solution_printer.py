d25847e0e00c4aa9c8cf0405f46a4e57731797ee
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-18 20:32:58 -0700

    Add solution printer class

---
+++ LEFA-scheduler/models/solution_printer.py
@@ -0,0 +1,60 @@
+from ortools.sat.python import cp_model
+
+class SolutionPrinter(cp_model.CpSolverSolutionCallback):
+
+	# def __init__(self, schedule, days, students, aircraft, hourly_blocks, limit):
+	def __init__(self, instructors, days, available_aircraft, schedule, limit, test_environment=False):
+		cp_model.CpSolverSolutionCallback.__init__(self)
+		self._instructors = instructors
+		self._days = days
+		self._available_aircraft = available_aircraft
+		self._schedule = schedule
+		self._solution_limit = limit
+		self._solution_count = 0
+		self.test_environment = test_environment
+		self.solution_log = { # solution log for testing
+			'days' : [],
+			'instructors' : [],
+			'students' : [],
+			'aircraft' : [],
+			'blocks' : [],
+			'day_and_hour_blocks' : []
+		}
+
+	def on_solution_callback(self):
+		# solution_log = { # solution log for testing
+		# 	'days' : set(),
+		# 	'instructors' : set(),
+		# 	'students' : set(),
+		# 	'aircraft' : set(),
+		# 	'blocks' : set(),
+		# 	'day_and_hour_blocks' : set()
+		# }
+		self._solution_count += 1
+		for day in self._days:
+			for instructor in self._instructors.values():
+				for student in instructor.students:
+					for aircraft_model, flight_configuration in student.aircraft.items():
+						for aircraft in self._available_aircraft[aircraft_model].values():
+							for schedule_block in aircraft.schedule_blocks:
+								if (day, instructor.full_name, student.full_name, aircraft.name, schedule_block) in self._schedule: # if the block is in the schedule
+									if self.Value(self._schedule[(day, instructor.full_name, student.full_name, aircraft.name, schedule_block)]): # if the value is true (valid solution)
+
+										self.solution_log['days'].append(day)
+										self.solution_log['instructors'].append(instructor.full_name)
+										self.solution_log['students'].append(student.full_name)
+										self.solution_log['aircraft'].append(aircraft.name)
+										self.solution_log['blocks'].append(schedule_block)
+										self.solution_log['day_and_hour_blocks'].append(day + '_' + str(schedule_block))
+
+										if not self.test_environment:
+											print('{},{},{},{},{}'.format(day, instructor.full_name, student.full_name, aircraft.name, schedule_block))
+		if self._solution_count >= self._solution_limit:
+			if not self.test_environment:
+				print('Stop search after %i solutions' % self._solution_limit)
+			self.StopSearch()
+
+
+
+	def solution_count(self):
+		return self._solution_count
