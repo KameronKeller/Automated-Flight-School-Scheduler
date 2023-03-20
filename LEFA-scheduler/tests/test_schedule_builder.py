ac10b16614693d4be07ac812dc678e275b7477fe
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-20 09:47:03 -0700

    Test one flight per day and one free block

--- LEFA-scheduler/tests/test_schedule_builder.py
+++ LEFA-scheduler/tests/test_schedule_builder.py
@@ -3,6 +3,7 @@ from ortools.sat.python import cp_model
 from models.schedule_builder import ScheduleBuilder
 from models.instructor import Instructor
 from tests.factory.instructor_factory import InstructorFactory
+from tests.factory.student_factory import StudentFactory
 from models.aircraft_factory import AircraftFactory
 from models.student import Student
 from models.calendar import Calendar
@@ -32,33 +33,39 @@ class TestScheduleBuilder(unittest.TestCase):
     # def test_flights_not_scheduled_when_instructor_unavailable(self):
 
     def test_student_has_one_flight_per_day_max(self):
-        student_unavailability = { # only free from 9-13 Sun-Tues
-            'Sunday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Monday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Tuesday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Wednesday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Thursday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Friday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
-            'Saturday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;'}
+        # student_unavailability = { # only free from 9-13 Sun-Tues
+        #     'Sunday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Monday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Tuesday' : '05:00;06:00;07:00;08:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Wednesday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Thursday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Friday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;',
+        #     'Saturday' : '05:00;06:00;07:00;08:00;09:00;10:00;11:00;12:00;13:00;14:00;15:00;16:00;17:00;18:00;19:00;20:00;'}
 
-        student = Student('Student', 'L', student_unavailability, 'Private', 'Rotor-Wing', self.free_instructor.full_name)
+        # student = Student('Student', 'L', student_unavailability, 'Private', 'Rotor-Wing', self.free_instructor.full_name)
+        student = StudentFactory.create_three_blocks_three_days_student(first_name='Student', last_name='L', rating='Private', schedule_type='Rotor-Wing', instructor=self.free_instructor.full_name)
         self.free_instructor.add_student(student)
         schedule_builder = ScheduleBuilder(self.free_instructors, Calendar.get_days(), self.available_aircraft, test_environment=True)
-        schedule_builder.generate_model()
-        schedule_builder.add_constraints()
-        status, solution_log = schedule_builder.output_schedule()
-        print(solution_log)
+        status, solution_log = schedule_builder.build_schedule()
+        # print(solution_log)
         # print(student.unavailability)
-        self.assertIn('Sunday', solution_log['days'])
-        self.assertIn('Monday', solution_log['days'])
-        self.assertIn('Tuesday', solution_log['days'])
+        # self.assertIn('Sunday', solution_log['days'])
+        # self.assertIn('Monday', solution_log['days'])
+        # self.assertIn('Tuesday', solution_log['days'])
         self.assertTrue(len(solution_log['days']) == len(set(solution_log['days'])))
         self.assertEqual(status, cp_model.FEASIBLE)
 
-        # assert False
+    def test_only_one_free_block_results_in_infeasable(self):
+        student = StudentFactory.create_one_block_student(first_name='Student', last_name='L', rating='Private', schedule_type='Rotor-Wing', instructor=self.free_instructor.full_name)
+        self.free_instructor.add_student(student)
+        schedule_builder = ScheduleBuilder(self.free_instructors, Calendar.get_days(), self.available_aircraft, test_environment=True)
+        status, solution_log = schedule_builder.build_schedule()
+        self.assertTrue(len(solution_log['days']) == len(set(solution_log['days'])))
+        self.assertEqual(status, cp_model.INFEASIBLE)
+
 
     # def test_instructor_has_one_student_at_a_given_time(self):
-    #     assert False
+
 
     # def test_solo_students_have_no_instructor(self):
     #     assert False
