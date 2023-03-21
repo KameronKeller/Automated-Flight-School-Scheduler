93f63ccc90ac238e60584480b0ef8da37c36ac83
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-21 15:58:30 -0700

    Add method to get_possible_blocks

--- LEFA-scheduler/models/calendar.py
+++ LEFA-scheduler/models/calendar.py
@@ -1,7 +1,13 @@
 class Calendar:
 
 	DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	EARLIEST_BLOCK = 7
+	LATEST_BLOCK = 17
 
 	@classmethod
 	def get_days(self):
 		return Calendar.DAYS
+
+	@classmethod
+	def get_possible_blocks(self):
+		return list(range(Calendar.EARLIEST_BLOCK, Calendar.LATEST_BLOCK + 1))
