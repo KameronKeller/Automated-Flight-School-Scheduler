2a5e4367dac362bd44861e91514125054e88a42b
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-21 08:04:37 -0700

    Add solo placeholder

--- LEFA-scheduler/models/instructor_student.py
+++ LEFA-scheduler/models/instructor_student.py
@@ -1,9 +1,10 @@
 from models.student import Student
 
 class InstructorStudent(Student):
-	def __init__(self, first_name, last_name, unavailability, rating, schedule_type, instructor, students):
+	def __init__(self, first_name, last_name, unavailability, rating, schedule_type, instructor, students, solo_placeholder=False):
 		super().__init__(first_name, last_name, unavailability, rating, schedule_type, instructor)
 		self.students = students
+		self.solo_placeholder = solo_placeholder
 
 	def add_student(self, student):
 		self.students.append(student)
