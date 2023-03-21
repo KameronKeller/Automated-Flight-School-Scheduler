a9a992104ae04984ba604f26195044955527ba31
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-21 08:04:29 -0700

    Add solo placeholder flag

--- LEFA-scheduler/models/instructor.py
+++ LEFA-scheduler/models/instructor.py
@@ -2,9 +2,10 @@ from models.person import Person
 
 class Instructor(Person):
 
-	def __init__(self, first_name, last_name, unavailability):
+	def __init__(self, first_name, last_name, unavailability, solo_placeholder=False):
 		super().__init__(first_name, last_name, unavailability)
 		self.students = []
+		self.solo_placeholder = solo_placeholder
 
 	def add_student(self, student):
 		self.students.append(student)
