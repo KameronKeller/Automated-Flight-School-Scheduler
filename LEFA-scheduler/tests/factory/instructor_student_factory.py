b066a9bf0abc44c5d1468fc6a477c391ce8f8f3c
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-21 13:04:45 -0700

    Refactor and add four blocks over three days instructor student

--- LEFA-scheduler/tests/factory/instructor_student_factory.py
+++ LEFA-scheduler/tests/factory/instructor_student_factory.py
@@ -1,21 +1,20 @@
 from models.instructor_student import InstructorStudent
+from tests.factory.unavailability_factory import UnavailabilityFactory
 
 class InstructorStudentFactory:
 
-	first_name = 'first'
-	last_name = 'last'
-	unavailability = {'fixedwing_friday_unavailability': '05:30;06:30;',
-	'fixedwing_monday_unavailability': '06:30;05:30;09:30;11:30;10:30;',
-	'fixedwing_saturday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_sunday_unavailability': '05:30;06:30;07:30;08:30;09:30;10:30;11:30;12:30;13:30;14:30;15:30;16:30;17:30;18:30;19:30;20:30;',
-	'fixedwing_thursday_unavailability': '05:30;06:30;',
-	'fixedwing_tuesday_unavailability': '06:30;05:30;',
-	'fixedwing_wednesday_unavailability': '05:30;06:30;'}
-	rating = 'rating'
-	schedule_type = 'schedule_type'
+	first_name = 'InstructorStudent'
+	last_name = 'L'
+
+	rating = 'Private'
+	schedule_type = 'Rotor-Wing'
 	instructor = 'instructor'
-	students = ['student_1']
+	students = []
 
 	@staticmethod
-	def create_instructor_student(first_name=first_name, last_name=last_name, unavailability=unavailability, rating=rating, schedule_type=schedule_type, instructor=instructor, students=students):
+	def create_instructor_student(first_name=first_name, last_name=last_name, unavailability=UnavailabilityFactory.get_default_unavailability(), rating=rating, schedule_type=schedule_type, instructor=instructor, students=students):
+		return InstructorStudent(first_name=first_name, last_name=last_name, unavailability=unavailability, rating=rating, schedule_type=schedule_type, instructor=instructor, students=students)
+
+	@staticmethod
+	def create_four_block_three_days_instructor_student(first_name=first_name, last_name=last_name, unavailability=UnavailabilityFactory.get_four_odd_blocks_three_days(), rating=rating, schedule_type=schedule_type, instructor=instructor, students=students):
 		return InstructorStudent(first_name=first_name, last_name=last_name, unavailability=unavailability, rating=rating, schedule_type=schedule_type, instructor=instructor, students=students)
