dc7819b2783874412883b858a4cb9aa6fd29fe8f
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-21 15:56:26 -0700

    Add create_one_day_free_only_instructor

--- LEFA-scheduler/tests/factory/instructor_factory.py
+++ LEFA-scheduler/tests/factory/instructor_factory.py
@@ -25,3 +25,7 @@ class InstructorFactory:
 	@staticmethod
 	def create_four_block_three_days_instructor(first_name=first_name, last_name=last_name, unavailability=UnavailabilityFactory.get_four_odd_blocks_three_days()):
 		return Instructor(first_name=first_name, last_name=last_name, unavailability=unavailability)
+
+	@staticmethod
+	def create_one_day_free_only_instructor(first_name=first_name, last_name=last_name, unavailability=UnavailabilityFactory.get_completely_free_one_day_only()):
+		return Instructor(first_name=first_name, last_name=last_name, unavailability=unavailability)
