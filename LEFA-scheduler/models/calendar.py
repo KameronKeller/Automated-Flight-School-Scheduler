6e0b69b780c38816cba97635e1ccb19fa2ef57a3
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-23 08:23:45 -0700

    Refactor to avoid constants

--- LEFA-scheduler/models/calendar.py
+++ LEFA-scheduler/models/calendar.py
@@ -1,13 +1,27 @@
 class Calendar:
 
 	DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-	EARLIEST_BLOCK = 7
-	LATEST_BLOCK = 17
+	
+	def __init__(self, earliest_block, latest_block, days=DAYS):
+		self.earliest_block = earliest_block
+		self.latest_block = latest_block
+		self.days = days
 
-	@classmethod
-	def get_days(self):
-		return Calendar.DAYS
+	def get_max_num_blocks_per_day(self):
+		return (self.latest_block + 2 - self.earliest_block) / 2
 
-	@classmethod
+	# @classmethod
+	# def get_days(self):
+	# 	return Calendar.DAYS
+
+	# @classmethod
+	# def get_earliest_block(self):
+	# 	return Calendar.EARLIEST_BLOCK
+
+	# @classmethod
+	# def get_latest_block(self):
+	# 	return Calendar.LATEST_BLOCK
+
+	# @classmethod
 	def get_possible_blocks(self):
-		return list(range(Calendar.EARLIEST_BLOCK, Calendar.LATEST_BLOCK + 1))
+		return set(range(self.earliest_block, self.latest_block + 1))
