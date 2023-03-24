0b20c515698985930c5f15df8b2bea99d619ebd2
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-24 07:40:14 -0700

    Remove comments

--- LEFA-scheduler/models/calendar.py
+++ LEFA-scheduler/models/calendar.py
@@ -10,18 +10,5 @@ class Calendar:
 	def get_max_num_blocks_per_day(self):
 		return (self.latest_block + 2 - self.earliest_block) / 2
 
-	# @classmethod
-	# def get_days(self):
-	# 	return Calendar.DAYS
-
-	# @classmethod
-	# def get_earliest_block(self):
-	# 	return Calendar.EARLIEST_BLOCK
-
-	# @classmethod
-	# def get_latest_block(self):
-	# 	return Calendar.LATEST_BLOCK
-
-	# @classmethod
 	def get_possible_blocks(self):
 		return set(range(self.earliest_block, self.latest_block + 1))
