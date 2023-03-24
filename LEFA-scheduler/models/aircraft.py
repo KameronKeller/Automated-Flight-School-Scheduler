3cf94bd24cd0d63ea84e96bf892a125d55c5293f
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-24 07:32:29 -0700

    Add soloable flag

--- LEFA-scheduler/models/aircraft.py
+++ LEFA-scheduler/models/aircraft.py
@@ -1,7 +1,8 @@
 class Aircraft:
 
-	def __init__(self, name, model, block_type, schedule_blocks):
+	def __init__(self, name, model, block_type, schedule_blocks, soloable=True):
 		self.name = name
 		self.model = model
 		self.block_type = block_type # odd or even
 		self.schedule_blocks = schedule_blocks
+		self.soloable = soloable
