db5cb22e4bb0232a65b7413475ee4c030ace2dab
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-24 07:34:16 -0700

    Add test environment flag, split rw and fw instructors, clean input, identify similar names

--- LEFA-scheduler/models/profile_builder.py
+++ LEFA-scheduler/models/profile_builder.py
@@ -1,12 +1,15 @@
 import csv
+import string
+from difflib import SequenceMatcher
 from models.instructor import Instructor
 from models.student import Student
 from models.instructor_student import InstructorStudent
 
 class ProfileBuilder:
 
-	def __init__(self, csv_path):
+	def __init__(self, csv_path, test_environment=True):
 		self.csv_path = csv_path
+		self.test_environment = test_environment
 		self.required_fieldnames = {
 			'ID',
 			'What is your first name?',
@@ -48,7 +51,9 @@ class ProfileBuilder:
 
 
 		profiles = self.create_profiles()
-		instructors = {}
+		instructors = None
+		fw_instructors = {}
+		rw_instructors = {}
 		students = set()
 		for profile in profiles:
 			full_name = profile['full_name']
@@ -56,29 +61,38 @@ class ProfileBuilder:
 				# unavailability = get_unavailability(profile)
 				if profile['schedule_type'] == 'Rotor-Wing':
 					unavailability = profile['rotorwing_unavailability']
+					rw_instructors[full_name] = Instructor(profile['first_name'], profile['last_name'], unavailability)
 				else:
 					unavailability = profile['fixedwing_unavailability']
+					fw_instructors[full_name] = Instructor(profile['first_name'], profile['last_name'], unavailability)
 
-				instructors[full_name] = Instructor(profile['first_name'], profile['last_name'], unavailability)
 			else:
 				students.add(profile['full_name']) # keep track of students for second pass
 
 		for profile in profiles:
+			profile_type = profile['schedule_type']
+			if profile_type == 'Rotor-Wing':
+				instructors = rw_instructors
+			else:
+				instructors = fw_instructors
+
 			instructor_name = profile['instructor']
 			if profile['instructor'] in instructors:
-				if profile['schedule_type'] == 'Rotor-Wing':
+				if profile_type == 'Rotor-Wing':
 					unavailability = profile['rotorwing_unavailability']
+					instructors = rw_instructors
 				else:
 					unavailability = profile['fixedwing_unavailability']
+					instructors = fw_instructors
 
 				if profile['full_name'] in instructors and profile['full_name'] in students:
-					# print('found the instructorStudents {}'.format(profile['full_name']))
-					instructor_student = InstructorStudent(profile['first_name'], profile['last_name'], unavailability, profile['current_rating'], profile['schedule_type'], instructors[instructor_name], instructors[profile['full_name']].students)
+					instructor_student = InstructorStudent(profile['first_name'], profile['last_name'], unavailability, profile['current_rating'], profile_type, instructors[instructor_name], instructors[profile['full_name']].students)
+
 					instructors[instructor_name].add_student(instructor_student)
 					instructors[profile['full_name']] = instructor_student
 				else:
 					# create the students
-					student = Student(profile['first_name'], profile['last_name'], unavailability, profile['current_rating'], profile['schedule_type'], instructors[instructor_name])
+					student = Student(profile['first_name'], profile['last_name'], unavailability, profile['current_rating'], profile_type, instructors[instructor_name])
 					
 					# if they need solo, add a 'solo instructor'
 					for aircraft_model, flight_configuration in student.aircraft.items():
@@ -92,13 +106,15 @@ class ProfileBuilder:
 					instructors[instructor_name].add_student(student)
 			else:
 				if instructor_name != "I am an instructor":
-					print("instructor not found!!!! {}".format(instructor_name))
+					name = profile['full_name']
+					if name != ' ':
+						print('{} needs an instructor'.format(name))
 
-		return instructors
+		return fw_instructors, rw_instructors
 
 	def create_profiles(self):
 		profiles = []
-
+		people = {}
 		# Note: encoding is required to handle special characters.
 		# https://stackoverflow.com/questions/19699367/for-line-in-results-in-unicodedecodeerror-utf-8-codec-cant-decode-byte
 		with open(self.csv_path, encoding = "ISO-8859-1") as csv_file:
@@ -110,12 +126,18 @@ class ProfileBuilder:
 			for row in csv_reader:
 				# print(row)
 
+				first_name = row['What is your first name?']
+				last_name = row['What is your last name?']
+				first_name = first_name.strip()
+				last_name = last_name.strip()
+				full_name = first_name + ' ' + last_name
+
 				profile = {
 					# may not need submission_id, but adding just-in-case
 					'submission_id' : row['ID'],
-					'first_name' : row['What is your first name?'],
-					'last_name' : row['What is your last name?'],
-					'full_name' : row['What is your first name?'] + ' ' + row['What is your last name?'],
+					'first_name' : first_name,
+					'last_name' : last_name,
+					'full_name' : full_name,
 					'phone_number' : row['What is your phone number?'],
 					'cocc_student' : row['Are you a COCC student?'],
 					'instructor' : row['Who is your instructor?'],
@@ -142,6 +164,26 @@ class ProfileBuilder:
 					},
 						'fixedwing_time_off_explaination' : row['Explanation of Time Off2']
 				}
+
+				if not self.test_environment:
+					name = profile['full_name']
+					if name != ' ':
+						instructor = profile['instructor']
+						if name not in people:
+							# Check similarity
+							for existing_name in people.keys():
+								similarity = self.similar(name, existing_name)
+								if similarity >= 0.85:
+									print('{} is very similar to {}, are they duplicates?'.format(name, existing_name))
+
+							people[name] = [instructor]
+						else:
+							people[name].append(instructor)
+							instructors = people[name]
+							if len(set(instructors)) == 1:
+								print('Duplicate found: {}, Instructors: {}'.format(name, people[name]))
+
+
 				profiles.append(profile)
 		return profiles
 
@@ -150,5 +192,6 @@ class ProfileBuilder:
 			if required_fieldname not in fieldnames:
 				raise KeyError('CSV file must contain required fieldnames')
 
-
+	def similar(self, a, b):
+		return SequenceMatcher(None, a, b).ratio()
 
