2556888a4af030640a1f88d9629a41c3342e6128
Kameron Keller <mrkameronkeller@gmail.com>
2023-03-24 07:37:12 -0700

    Commit before deletion

---
+++ LEFA-scheduler/testing.py
@@ -0,0 +1,13 @@
+# class Instructor:
+
+# 	def __init__(self):
+# 		students = ['a', 'b', 'c']
+
+
+
+# i1 = Instructor()
+# i2 = Instructor()
+
+# instructors = [i1, i2]
+
+# print("{} {}".format(i, s) for i in instructors for s in instructor.students)
